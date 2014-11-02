6c9a81662c1ce6b52994689766e458ea6bd36c5b
Jimmy Koene <jimmykoene216@gmail.com>
2014-11-02 09:18:20 +0100

    Fixed bug Peacefulpack sappling

--- PeacefulPack/pp_common/com/wuppy/peacefulpackmod/PeacefulPack.java
+++ PeacefulPack/pp_common/com/wuppy/peacefulpackmod/PeacefulPack.java
@@ -19,7 +19,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(modid = PeacefulPack.modid, name = "Peacefulpack", version = "1.1.8.4")
+@Mod(modid = PeacefulPack.modid, name = "Peacefulpack", version = "1.1.8.5")
 public class PeacefulPack
 {
 	public static final String modid = "wuppy29_peacefulpack";
--- PeacefulPack/pp_common/com/wuppy/peacefulpackmod/block/BlockBlazeSapling.java
+++ PeacefulPack/pp_common/com/wuppy/peacefulpackmod/block/BlockBlazeSapling.java
@@ -2,25 +2,51 @@ package com.wuppy.peacefulpackmod.block;
 
 import java.util.Random;
 
-import net.minecraft.block.BlockBush;
+import net.minecraft.block.Block;
 import net.minecraft.block.IGrowable;
+import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
 import com.wuppy.peacefulpackmod.PeacefulPack;
 import com.wuppy.peacefulpackmod.worldgen.WorldGenBlazeTrees;
 
-public class BlockBlazeSapling extends BlockBush implements IGrowable
+public class BlockBlazeSapling extends Block implements IGrowable
 {
     public BlockBlazeSapling()
     {
-        super();
+        super(Material.plants);
         setStepSound(soundTypeGrass);
         setHardness(0.0F);
         setBlockName("blazeSapling");
         setBlockTextureName(PeacefulPack.modid + ":" + "blazeSapling");
+        setBlockBounds(0.1F, 0.0F, 0.1F, 0.9F, 0.8F, 0.9F);
         setCreativeTab(PeacefulPack.ppBlocksTab);
+    }
+    
+    public boolean renderAsNormalBlock()
+    {
+        return false;
+    }
+
+    /**
+     * The type of render function that is called for this block
+     */
+    public int getRenderType()
+    {
+        return 1;
+    }
+    
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }
+    
+    public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_)
+    {
+        return null;
     }
     
     @Override
@@ -37,7 +63,7 @@ public class BlockBlazeSapling extends BlockBush implements IGrowable
     {
         super.updateTick(world, i, j, k, random);
         
-        if(world.getBlockLightValue(i, j + 1, k) >= 1 && random.nextInt(7) == 0)
+        if(world.getBlockLightValue(i, j + 1, k) < 7 && random.nextInt(7) == 0)
         {
             int l = world.getBlockMetadata(i, j, k);
             if((l & 8) == 0)
